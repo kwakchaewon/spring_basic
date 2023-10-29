7819112b9aa1fbc995b87dbc9adf7dac3946399b
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2023-10-30 03:55:19 +0900

    JDBC DB 연결

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,25 @@
 package hello.hellospring;
 
+import com.sun.jdi.PrimitiveValue;
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    
+    // 데이터베이스 연결 객체 생성
+    private final DataSource dataSource;
+    
+    // 데이터소스 주입
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,7 +28,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return  new MemoryMemberRepository();
+//        return  new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 
