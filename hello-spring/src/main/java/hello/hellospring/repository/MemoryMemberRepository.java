7e529d2c392f679ece18dc5d5852808e2eeaa573
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2023-10-30 20:41:15 +0900

    Update MemoryMemberRepository.java
    주석 변경

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -15,29 +15,29 @@ public class MemoryMemberRepository implements MemberRepository{
     private  static long sequence = 0L;
 
     @Override
-    // 멤버 저장 로직
+    // 1. 멤버 저장 로직
     public Member save(Member member) {
-        member.setId(++sequence); // 아이디 setting & 시퀀스 값 +1
-        store.put(member.getId(), member); // id값과 멤버 객체 저장
-        return member; // 멤버 객체 반환
+        member.setId(++sequence); // 1) 아이디 setting & 시퀀스 값 +1
+        store.put(member.getId(), member); // 2) id값과 멤버 객체 저장
+        return member; // 3) 멤버 객체 반환
     }
 
     @Override
-    // id로 검색 로직
+    // 2. id로 검색 로직
     public Optional<Member> findById(Long id) {
         return Optional.ofNullable(store.get(id));
     }
 
     @Override
-    // Name으로 검색 로직
+    // 3. Name으로 검색 로직
     public Optional<Member> findByName(String name) {
-        return store.values().stream() // 1. 스토어에서 루프를 돌림
-                .filter(member -> member.getName().equals(name)) // 2. Lambda 사용: member Name이 파라미터 name과 같은지 확인
-                .findAny(); // 3. 찾으면 반환
+        return store.values().stream() // 1) 스토어에서 루프를 돌림
+                .filter(member -> member.getName().equals(name)) // 2) Lambda 사용: member Name이 파라미터 name과 같은지 확인
+                .findAny(); // 3) 찾으면 반환
     }
 
     @Override
-    // 전체 멤버 반환
+    // 4. 전체 멤버 반환
     public List<Member> findAll() {
         return new ArrayList<>(store.values()); // store 의 values() : 모든 values 반환
     }
